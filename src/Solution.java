452ea60907be4eba497b6d79b3598adf812ac70d
zzxishero <2445459509@qq.com>
2019-04-09 23:44:26 +0800

    Initial commit

---
+++ src/Solution.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+
+        while(head != null && head.val == val) {
+            ListNode delNode = head;
+            head = head.next;
+            delNode = null;
+        }
+
+        if(head == null) {
+            return null;
+        }
+
+        ListNode prev = head;
+        while(prev.next != null) {
+            if(prev.next.val == val) {
+                ListNode delNode = prev.next;
+                prev.next = delNode.next;
+                delNode = null;
+            }else{
+                prev = prev.next;
+            }
+        }
+        return head;
+    }
+};
