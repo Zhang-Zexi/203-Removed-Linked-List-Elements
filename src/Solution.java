e0cfd29154665dc18053463597264b619118c483
zhangzx <zhangzx@allinfinance.com>
2019-04-15 10:22:59 +0800

    添加测试用例

--- src/Solution.java
+++ src/Solution.java
@@ -33,4 +33,14 @@ class Solution {
         }
         return head;
     }
+
+    public static void main(String[] args) {
+
+        int[] nums = {1, 2, 3, 4, 5, 6};
+        ListNode head = new ListNode(nums);
+        System.out.println(head);
+
+        ListNode res = (new Solution()).removeElements(head, 6);
+        System.out.println(res);
+    }
 };
